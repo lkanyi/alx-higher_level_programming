53bcb2eecbffbaab7b785b59770af086e2142803
leah kanyi <kanyileah46@gmail.com>
2022-06-03 00:25:09 +0300

    updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num = len(sys.argv)
-    if num == 1:
-        print("{} arguments.".format(num - 1))
-    elif num == 2:
-        print("{} argument:".format(num - 1))
-    else:
-        print("{} arguments:".format(num - 1))
-
-    for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+    lst = sys.argv
+    lth = len(lst) - 1
+    arg = "argument"
+    print("{} argument{}{}".format(
+        lth,
+        "" if lth == 1 else "s",
+        "." if lth == 0 else ":"))
+    for i in range(1, len(lst)):
+        print("{}: {}".format(i, lst[i]))
