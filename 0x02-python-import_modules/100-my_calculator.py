bbc96cc77e40334831772352e57ccf59c8aefb06
leah kanyi <kanyileah46@gmail.com>
2022-06-03 01:25:32 +0300

    import modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
+from sys import argv
+from calculator_1 import add, sub, mul, div
 
-    num_args = len(sys.argv)
-    if num_args != 4:
+if __name__ == "__main__":
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
 
-    if op is '+':
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op is '-':
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op is '*':
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op is '/':
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+    ops = ["+", "-", "*", "/"]
+    fcs = [add, sub, mul, div]
+    for i in range(0, 4):
+        if argv[2] == ops[i]:
+            a = int(argv[1])
+            b = int(argv[3])
+            print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, fcs[i](a, b)))
+            exit(0)
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
