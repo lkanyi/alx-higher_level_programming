3493d22a879e03e20b626e4f3da19692d0e3260f
leah kanyi <kanyileah46@gmail.com>
2022-06-08 22:19:23 +0300

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,9 @@
- #!/usr/bin/python3
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return (list((replace if e == search else e) for e in my_list))
+    copy = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            copy.append(replace)
+        else:
+            copy.append(my_list[i])
+    return copy
